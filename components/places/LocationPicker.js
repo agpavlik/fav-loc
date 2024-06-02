75e977cfde92d1b86954dc6be33e6def55034e2c
agpavlik <agpavlik@gmail.com>
2024-06-02 15:33:47 -0400

    Implement permission logic

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -3,12 +3,54 @@ import { View, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton";
 
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
+
 // Will offer user two ways of picking a location.
 // Either by locating the user through GPS, or
 // by allowing the user to pick a location on a map.
 
 function LocationPicker() {
-  function getLocationHandler() {}
+  // ---- Permission is required for both devices android and iOS
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+  // ----
+
+  // ---- Get location through GPS
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+    // Call verifyPermission and get back true or false
+    // telling whether app have the permission or not.
+    // If there is no permission, returned because can't continue
+    // Otherwise will continue and get the position of a user.
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
 
   function pickOnMapHandler() {}
 
